b736d85396842dee7222c732b30be32d3e92ac81
svrao <avgindgamer@gmail.com>
2022-01-06 20:55:39 +0530

    private NAT blocked

--- NAT.tf
+++ NAT.tf
@@ -11,10 +11,10 @@ resource "aws_nat_gateway" "public_NAT" {
   depends_on = [aws_internet_gateway.igw]
 }
 
-resource "aws_nat_gateway" "private_NAT_1a" {
-  connectivity_type = "private"
-  subnet_id = aws_subnet.private_1a.id
-}
+# resource "aws_nat_gateway" "private_NAT_1a" {
+#   connectivity_type = "private"
+#   subnet_id = aws_subnet.private_1a.id
+# }
 
 # resource "aws_nat_gateway" "private_NAT_1b" {
 #   connectivity_type = "private"
