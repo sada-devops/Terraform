99d269fc19ea68c43f2ca0c3b7eee1023f80824f
svrao <avgindgamer@gmail.com>
2022-01-06 20:50:02 +0530

    edited NAT, added s3 backend

--- NAT.tf
+++ NAT.tf
@@ -1,3 +1,16 @@
+resource "aws_nat_gateway" "public_NAT" {
+  allocation_id = aws_eip.eip_1.id
+  subnet_id     = aws_subnet.private_1a.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
+
 resource "aws_nat_gateway" "private_NAT_1a" {
   connectivity_type = "private"
   subnet_id = aws_subnet.private_1a.id
