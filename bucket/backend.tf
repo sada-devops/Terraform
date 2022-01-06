0a464f95c07aed958085baf602d6a202d039c235
svrao <avgindgamer@gmail.com>
2022-01-06 21:24:59 +0530

    b

--- bucket/backend.tf
+++ bucket/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "my-tf-test-bucket"
-    key    = "terraform/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "my-tf-test-bucket"
+#     key    = "terraform/terraform.tfstate"
+#     region = "ap-south-1"
+#   }
+#}
